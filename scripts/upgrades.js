f314645e0e8cf264ae0ab131a5a97e778111e48a
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2024-07-28 18:26:26 -0500

    changed update look

--- scripts/upgrades.js
+++ scripts/upgrades.js
@@ -10,21 +10,28 @@ class AluminumAutoMiner {
     constructor() {
         this.html = document.querySelector('#aluminumAutoMiner');
 
-        this.buyBtn = this.html.children[1].firstElementChild.firstElementChild.childNodes[1];
-        this.priceToken = this.buyBtn.childNodes[1];
-        this.levelToken = this.html.children[0].children[2];
+        this.image = document.querySelector('#aluminumAutoMiner .upgrade__img');
+        this.description = document.querySelector('#aluminumAutoMiner .upgrade__description');
+
+        this.priceToken = document.querySelector('#aluminumAutoMiner .upgrade__price');
+        this.levelToken = document.querySelector('#aluminumAutoMiner .upgrade__level');
 
         this.price = 5;
         this.priceMultiplier = 2.5;
         this.aluminumPerInterval  = 1;
         this.aluminumGrowthRate = 2;
         this.level = 0;
+        this.maxLevel = 10;
 
-        this.buyBtn.addEventListener("click", this.buy);
+        this.html.addEventListener("click", this.buy);
+        this.image.addEventListener("click", (e) => {
+          this.showDescription();
+          e.stopPropagation();
+        });
     }
     
     buy = () => {
-      if(this.price <= money) {
+      if(this.price <= money && this.level < this.maxLevel) {
         topStats.changeMoneyAmount(-this.price);
         this.price *= this.priceMultiplier;
         this.price = Math.floor(this.price * 10) / 10;
@@ -32,6 +39,10 @@ class AluminumAutoMiner {
         this.upgrade();
         this.updateLevelToken();
       }
+    }
+
+    showDescription = () => {
+      this.description.style.display = (this.description.style.display === "none") ? "block" : "none";
     }
 
     upgrade() {
